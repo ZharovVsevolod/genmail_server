b59f9d3576036dad9e850c85f892debcac2ab76f
diduk001 <didurenkos@gmail.com>
2026-01-23 12:46:50 +0300

    fixed gmservices

--- gm_services/gm_services/config.py
+++ gm_services/gm_services/config.py
@@ -5,29 +5,37 @@ Most of the time you'll need the `Settings` variable with configuration specific
 
 You can run this .py file to print the config file and check if it is loaded correctly.
 """
+
 from pathlib import Path
 from pydantic import BaseModel
 from omegaconf import OmegaConf, DictConfig
+from os import getcwd
+import os.path
 
 from typing import Literal
+from dotenv import load_dotenv
 
 import logging
+
 logger = logging.getLogger(__name__)
 
-from dotenv import load_dotenv
-load_dotenv()
+
+load_dotenv(os.path.join(getcwd(), ".env"))
 
 
 # Typing parameters
-LLM_MODEL_TYPE = Literal["qwen3:14b", "qwen3:1.7b", "qwen3:8b", "qwen3:4b"]
+LLM_MODEL_TYPE = Literal[
+    "qwen3:14b", "qwen3:1.7b", "qwen3:8b", "qwen3:4b", "gpt-oss:20b-cloud"
+]
 EMBEDDINGS_MODEL_TYPE = Literal["FRIDA", "e5-large"]
 DEVICE_TYPE = Literal["cpu", "cuda", "mps"]
 LLM_ANSWER_PARSER_TYPE = Literal["none", "json", "string"]
 
 
 # Get the path to the yaml config file
-pwd = Path(__file__).parent.parent
-config_file = pwd / "config" / "config.yaml"
+pwd = getcwd()
+config_file = os.path.join(pwd, "config", "config.yaml")
+
 
 # ---------------------------------
 # The whole configuration structure
@@ -40,7 +48,7 @@ class Docs(BaseModel):
     @property
     def full_mail_path(self) -> str:
         return self.main_dir + self.mail_dir_path
-    
+
     @property
     def full_final_path(self) -> str:
         return self.main_dir + self.final_name
@@ -121,19 +129,19 @@ class Config(BaseModel):
 # Load the yaml config file
 def _load_yaml_config(path: Path) -> DictConfig:
     """Load the yaml config file from the specific path.
-    
+
     This function using OmegaConf for yaml hierarchical structure opportunity.
-    
+
     Arguments
     ---------
     path: Path
         Path to the .yaml config file
-    
+
     Returns
     -------
     config: DictConfig
         The config dictionary
-    
+
     Raises
     ------
     FileNotFoundError
@@ -147,18 +155,18 @@ def _load_yaml_config(path: Path) -> DictConfig:
         logger.exception(message)
         raise FileNotFoundError(error, message) from error
 
+
 Settings = Config(**_load_yaml_config(config_file))
 
 
 # Set logging point
 logging.basicConfig(
-    filename = Settings.system.logging_file,
-    level = Settings.system.logging_level,
-    encoding = "utf-8"
+    filename=Settings.system.logging_file,
+    level=Settings.system.logging_level,
+    encoding="utf-8",
 )
-
 
 
 if __name__ == "__main__":
     # We can run this .py file to check if the Settings was loaded correctly
-    print(Settings.model_dump_json(indent = 2))
+    print(Settings.model_dump_json(indent=2))
--- gm_services/gm_services/neural/llm/chain_config.py
+++ gm_services/gm_services/neural/llm/chain_config.py
@@ -1,21 +1,28 @@
-from ..databases.graphstore import Neo4jHandler
-from ..databases.vectorstore import ElasticHandler
+# TODO: fix (currently no graphbase in gm_services)
 
 
-def make_config_for_chain(
-    session_id: str, 
-    graphbase: Neo4jHandler | None = None,
-    vectorbase: ElasticHandler | None = None
-) -> dict:
-    """Config for runnable_chain.
-    
-    Contains everything that llm may need due process
-    """
-    chain_config = {
-        "configurable": {
-            "session_id": session_id,
-            "graphbase": graphbase,
-            "vectorbase": vectorbase
-        }
-    }
-    return chain_config
+def make_config_fot_chain(*args, **kwargs):
+    pass
+
+
+# from ..databases.graphstore import Neo4jHandler
+# from ..databases.vectorstore import ElasticHandler
+
+
+# def make_config_for_chain(
+#     session_id: str,
+#     graphbase: Neo4jHandler | None = None,
+#     vectorbase: ElasticHandler | None = None
+# ) -> dict:
+#     """Config for runnable_chain.
+
+#     Contains everything that llm may need due process
+#     """
+#     chain_config = {
+#         "configurable": {
+#             "session_id": session_id,
+#             "graphbase": graphbase,
+#             "vectorbase": vectorbase
+#         }
+#     }
+#     return chain_config
